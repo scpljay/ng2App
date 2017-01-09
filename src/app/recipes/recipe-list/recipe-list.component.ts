4cfc1db8e7eb456e2466f9e0ae7bdf541df08282
scpl.jay <scpl.jay@gmail.com>
2017-01-10 01:29:42 +0530

    Add Shopping List and Ingredient Class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,14 @@ import { RecipeItemComponent } from './recipe-item.component';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-	recipes: Recipe[] = [];
-	@Output() recipeSelected = new EventEmitter<Recipe>();
-	recipe = new Recipe('Dummy Name', 'Dummy Description', 'http://ersuk.co.uk/ekmps/shops/express2011/images/female-tailor-dummy-with-tripod-base-round-top-511-p.jpg');
+	recipes: Recipe[] = [
+		new Recipe('Paneer Tikka', 'Very hot and Tasty Sizler', 'https://i.ytimg.com/vi/LSfU5eVaAF4/maxresdefault.jpg', []);
 
+		new Recipe('Prawn', 'Very hot and Tasty Prawn Sizler', 'http://im.whatshot.in/event/2015/Jun/1434696706-tiger-prawn-with-carrot-french-beans.jpg', []);
+	];
+	@Output() recipeSelected = new EventEmitter<Recipe>();
+	/*recipe = new Recipe('Dummy Name', 'Dummy Description', 'http://ersuk.co.uk/ekmps/shops/express2011/images/female-tailor-dummy-with-tripod-base-round-top-511-p.jpg');
+*/
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ShoppingListAddComponent } from './shopping-list-add.component';
+import { Ingredient } from '../recipes/ingredient';
+
+@Component({
+  selector: 'fa-shopping-list',
+  templateUrl: './shopping-list.component.html'
+})
+export class ShoppingListComponent implements OnInit {
+	items: Ingredient[] = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
